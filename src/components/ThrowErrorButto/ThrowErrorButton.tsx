1da7debf1be2b80c777bef3689581d8d874e3c78
Olha Popova <fc.odessa.10@gmail.com>
2025-02-04 23:32:39 -0800

    refactor: change throwerrorbutton component to functional

--- src/components/ThrowErrorButto/ThrowErrorButton.tsx
+++ src/components/ThrowErrorButto/ThrowErrorButton.tsx
@@ -1,19 +1,16 @@
-import { Component } from 'react';
 import styles from './ThrowErrorButton.module.css';
 
-class ThrowErrorButton extends Component {
-  throwError = (): void => {
+const ThrowErrorButton: React.FC = () => {
+  const throwError = (): void => {
     console.error('Test Console Error: Something went wrong!');
     throw new Error('Test Error: Button Click Error!');
   };
 
-  render(): JSX.Element {
-    return (
-      <button className={styles.throwErrorButton} onClick={this.throwError}>
-        Throw Error
-      </button>
-    );
-  }
-}
+  return (
+    <button className={styles.throwErrorButton} onClick={throwError}>
+      Throw Error
+    </button>
+  );
+};
 
 export default ThrowErrorButton;
