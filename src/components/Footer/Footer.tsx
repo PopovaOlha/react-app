86dd114b94b2117f286f8e05295a871fbc34bf91
Olha Popova <fc.odessa.10@gmail.com>
2025-02-04 23:20:19 -0800

    refactor: change footer component to functional

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,16 @@
-import { Component } from 'react';
 import styles from './Footer.module.css';
 import logo from '../../assets/rss-logo.c19ce1b4.svg';
 import ThrowErrorButton from '../ThrowErrorButto/ThrowErrorButton';
 
-class Footer extends Component {
-  render(): JSX.Element {
-    return (
-      <footer className={styles.footer}>
-        <span className={styles.logo}>
-          <img src={logo} alt="React School logo" width={40} height={30} />
-        </span>
-        <ThrowErrorButton />
-      </footer>
-    );
-  }
-}
+const Footer: React.FC = () => {
+  return (
+    <footer className={styles.footer}>
+      <span className={styles.logo}>
+        <img src={logo} alt="React School logo" width={40} height={30} />
+      </span>
+      <ThrowErrorButton />
+    </footer>
+  );
+};
 
 export default Footer;
