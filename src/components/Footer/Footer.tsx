c3f7e2ab9676f0863ff688985e13dce6dd2dce1e
Olha Popova <fc.odessa.10@gmail.com>
2025-02-18 23:52:32 -0800

    refactor: store logo width and height in constants for better maintainability

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,18 @@
 import styles from './Footer.module.css';
 import logo from '../../assets/rss-logo.c19ce1b4.svg';
 import ThrowErrorButton from '../ThrowErrorButto/ThrowErrorButton';
+import { LOGO_HEIGHT, LOGO_WIDTH } from '../../config/constants';
 
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <span className={styles.logo}>
-        <img src={logo} alt="React School logo" width={40} height={30} />
+        <img
+          src={logo}
+          alt="React School logo"
+          width={LOGO_WIDTH}
+          height={LOGO_HEIGHT}
+        />
       </span>
       <ThrowErrorButton />
     </footer>
---
+++ src/config/constants.ts
@@ -0,0 +1,2 @@
+export const LOGO_WIDTH = 40;
+export const LOGO_HEIGHT = 30;
