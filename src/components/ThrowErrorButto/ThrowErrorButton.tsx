d63e40f0e7b487503cca2707d9387194bc410920
Olha Popova <fc.odessa.10@gmail.com>
2025-02-19 23:22:38 -0800

    fix: throw actual error instead of logging to console

--- src/components/ThrowErrorButto/ThrowErrorButton.tsx
+++ src/components/ThrowErrorButto/ThrowErrorButton.tsx
@@ -2,8 +2,9 @@ import styles from './ThrowErrorButton.module.css';
 
 const ThrowErrorButton: React.FC = () => {
   const throwError = (): void => {
-    console.error('Test Console Error: Something went wrong!');
+    throw new Error('Test Error: Something went wrong!');
   };
+
   return (
     <button className={styles.throwErrorButton} onClick={throwError}>
       Throw Error
